faa723ddf82fefdadd9c440a063bbaec8dd2861d
Jorge S. Cuesta <jckpo2@gmail.com>
2024-04-10 14:54:21 -0400

    Fixed logger and added base mongodb connection

--- packages/go/logger/adapter.go
+++ packages/go/logger/adapter.go
@@ -8,20 +8,16 @@ import (
 // ZerologAdapter represents an adapter for the Zerolog logging library.
 // This adapter allows to use zerolog in one of the following ways:
 //
-// logger := app.GetActivityLogger(ActivityName, ctx, params)
-// logger := app.GetActivityLogger(ActivityName, ctx, nil)
-// logger := app.GetWorkflowLogger(WorkflowName, ctx, params)
-// logger := app.GetWorkflowLogger(WorkflowName, ctx, nil)
+// l := app.GetActivityLogger(ActivityName, ctx, logger.NewFieldsFromStruct(params))
+// l := app.GetActivityLogger(ActivityName, ctx, nil)
+// l := app.GetWorkflowLogger(WorkflowName, ctx, logger.NewFieldsFromStruct(params))
+// l := app.GetWorkflowLogger(WorkflowName, ctx, nil)
 //
-// so the logger will or not have additional context properties (params) plus name and type [activity or workflow]
+// so the logger will or not have additional context properties (params)
 // and these are the ways you can use it on your activity or workflow
-// logger.Info("freaking awesome...")
-// logger.Info("freaking awesome...", app.Fields{"foo": 1, "bar": ""})
-// logger.InfoEvent().
-//
-//	Int("foo", 1).
-//	Str("bar", "").
-//	Msg("freaking awesome...")
+// l.Info("foo", logger.NewFieldsFromStruct(logger.Fields{"foo": "bar"}).GetLoggerFields()...)
+// l.Info("foo", logger.NewFieldsFromStruct(anyStructInstance).GetLoggerFields()...)
+// l.Info("bar", "foo", "1", "bar", 1)
 type ZerologAdapter struct {
 	logger zerolog.Logger
 }
@@ -37,19 +33,9 @@ func (zl *ZerologAdapter) Info(msg string, kvs ...interface{}) {
 	zl.logger.Info().Fields(utils.KeyValToMap(kvs...)).Msg(msg)
 }
 
-// InfoEvent returns a pointer to a zerolog.Event for logging info messages.
-func (zl *ZerologAdapter) InfoEvent() *zerolog.Event {
-	return zl.logger.Info()
-}
-
 // Debug This is the adapter function for zerolog's Debug().
 func (zl *ZerologAdapter) Debug(msg string, kvs ...interface{}) {
 	zl.logger.Debug().Fields(utils.KeyValToMap(kvs...)).Msg(msg)
-}
-
-// DebugEvent returns a pointer to a zerolog.Event for logging debug messages.
-func (zl *ZerologAdapter) DebugEvent() *zerolog.Event {
-	return zl.logger.Debug()
 }
 
 // Error This is the adapter function for zerolog's Error().
@@ -75,34 +61,7 @@ func (zl *ZerologAdapter) Error(msg string, kvs ...interface{}) {
 	logEvt.Fields(f).Msg(msg)
 }
 
-// ErrorEvent returns a pointer to a zerolog.Event for logging error messages.
-func (zl *ZerologAdapter) ErrorEvent() *zerolog.Event {
-	return zl.logger.Error()
-}
-
 // Warn This is the adapter function for zerolog's Warn().
 func (zl *ZerologAdapter) Warn(msg string, kvs ...interface{}) {
 	zl.logger.Warn().Fields(utils.KeyValToMap(kvs...)).Msg(msg)
-}
-
-// WarnEvent returns a pointer to a zerolog.Event for logging warning messages.
-func (zl *ZerologAdapter) WarnEvent() *zerolog.Event {
-	return zl.logger.Warn()
-}
-
-// WithContext This method creates a new ZerologAdapter with additional context fields.
-// The "name" parameter specifies the name of the context.
-// The "componentType" parameter specifies the type of the component (e.g., "activity", "workflow").
-// The "params" parameter is optional and can be used to add additional key-value pairs to the context.
-// The method returns a new ZerologAdapter instance with the specified context fields.
-func (zl *ZerologAdapter) WithContext(name, componentType string, params ...interface{}) *ZerologAdapter {
-	lCtx := zl.logger.With().
-		Str("name", name).
-		Str("component", componentType)
-
-	if params != nil {
-		lCtx = lCtx.Fields(utils.KeyValToMap(params...))
-	}
-
-	return &ZerologAdapter{logger: lCtx.Logger()}
 }
