72ea1b90523034adf2ab59e3eff4659ab0808fa5
Ramiro R. C <rawthil@gmail.com>
2024-04-15 10:20:23 -0300

    Go add pocket rpc package (#23)
    * Added Pocket RPC as Package.
    Implemented GetApp.
    TODO: Implement any other endpoints that we may need to use from the Pocket v0 RPC
    
    * Refactored `DoRRLoadBalanced` method to only apply backoff strategy when the client's list is less than MaxRetries
    
    * Added GetNodes to retrieve all the nodes from blockchain that also without bother with the pagination to the caller. It also allows filtering by service in case it is needed.
    
    * Enhance backoff logic. Replace Nodes output with pocket-go package one.
    
    ---------
    
    Co-authored-by: Jorge S. Cuesta <jckpo2@gmail.com>

---
+++ packages/go/pocket_rpc/go.mod
@@ -0,0 +1,13 @@
+module pocket_rpc
+
+go 1.21.6
+
+require github.com/pokt-foundation/pocket-go v0.17.0
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/zerolog v1.32.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+)
---
+++ packages/go/pocket_rpc/mock_rpc.go
@@ -0,0 +1,55 @@
+package pocket_rpc
+
+import (
+	"errors"
+	poktGoSdk "github.com/pokt-foundation/pocket-go/provider"
+	"github.com/stretchr/testify/mock"
+)
+
+const UnexpectedResponseType = "one of the response arguments does not match the expected type"
+
+type MockRpc struct {
+	mock.Mock
+}
+
+func NewMockRpc() *MockRpc {
+	mockRpc := MockRpc{}
+	return &mockRpc
+}
+
+func (rpc *MockRpc) GetClientPool() *ClientPool {
+	return nil
+}
+
+func (rpc *MockRpc) SetClientPool(_ *ClientPool) {
+}
+
+func (rpc *MockRpc) GetApp(address string) (*poktGoSdk.App, error) {
+	args := rpc.Called(address)
+	firstResponseArg := args.Get(0)
+	var response *poktGoSdk.App
+	if firstResponseArg != nil {
+		if v, ok := firstResponseArg.(*poktGoSdk.App); !ok {
+			return nil, errors.New(UnexpectedResponseType)
+		} else {
+			response = v
+		}
+	}
+
+	return response, args.Error(1)
+}
+
+func (rpc *MockRpc) GetNodes(height int64, service string) (nodes []*poktGoSdk.Node, e error) {
+	args := rpc.Called(height, service)
+	firstResponseArg := args.Get(0)
+	var response []*poktGoSdk.Node
+	if firstResponseArg != nil {
+		if v, ok := firstResponseArg.([]*poktGoSdk.Node); !ok {
+			return nil, errors.New(UnexpectedResponseType)
+		} else {
+			response = v
+		}
+	}
+
+	return response, args.Error(1)
+}
