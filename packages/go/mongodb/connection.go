54f7c92e5293c05d4b077311c2f12992d31c6d7c
Jorge S. Cuesta <jckpo2@gmail.com>
2024-04-16 17:32:19 -0400

    Enhance MongoDB Package. Added CloseConnection and Collection initialization.

--- packages/go/mongodb/connection.go
+++ packages/go/mongodb/connection.go
@@ -5,10 +5,27 @@ import (
 	"github.com/rs/zerolog"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"net/url"
 	"time"
 )
 
-func Initialize(uri string, l *zerolog.Logger) (client *mongo.Client) {
+func GetDatabaseName(uri string, defaultName string) string {
+	u, err := url.Parse(uri)
+	if err != nil {
+		panic(err)
+	}
+
+	// The database name resides in the path, trim the leading slash
+	dbName := u.Path[1:]
+
+	if dbName == "" {
+		return defaultName
+	}
+
+	return dbName
+}
+
+func Initialize(uri string, collections []string, l *zerolog.Logger) (client *mongo.Client, collectionsMap map[string]*mongo.Collection) {
 	var err error
 	// Set client options
 	clientOptions := options.Client().ApplyURI(uri)
@@ -31,5 +48,29 @@ func Initialize(uri string, l *zerolog.Logger) (client *mongo.Client) {
 
 	l.Info().Msg("Connected to MongoDB!")
 
+	_db := client.Database(GetDatabaseName(uri, "test"))
+	collectionsMap = make(map[string]*mongo.Collection)
+	for _, collectionName := range collections {
+		collection := _db.Collection(collectionName)
+		if e := _db.CreateCollection(ctx, collectionName); e != nil {
+			if err.(mongo.CommandError).Name != "NamespaceExists" {
+				l.Fatal().Err(err).Str("collection", collectionName).Msg("Errored preparing collection")
+			}
+		}
+
+		collectionsMap[collectionName] = collection
+	}
+
 	return
+}
+
+func CloseConnection(client *mongo.Client, l *zerolog.Logger) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			l.Fatal().Err(err).Msg("Errored closing MongoDB connection")
+		}
+		l.Info().Msg("MongoDB connection successfully closed.")
+	}()
 }
