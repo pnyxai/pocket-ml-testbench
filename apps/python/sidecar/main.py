f2a8930904563ad04636ca42d6905670b4a9e643
Ramiro R. C <rawthil@gmail.com>
2024-06-03 18:26:32 -0300

    Added sidecar for tokenizer endpoint in llm nodes (#56)
    * Added sidecar for tokenizer endpoint in llm nodes
    
    * removed test code
    
    * changed endpoint path

---
+++ apps/python/sidecar/main.py
@@ -0,0 +1,47 @@
+import os
+import time
+
+from app.app import get_app_logger, setup_app
+from app.config import read_config
+from fastapi import FastAPI
+from tokenizers_utils.load import prepare_tokenizer_data
+
+###################################################
+# SET UP SIDECAR
+###################################################
+cfg = read_config()
+
+app_config = setup_app(cfg)
+
+config = app_config["config"]
+
+l = get_app_logger("sidecar")
+l.info("starting sidecar")
+
+# Read tokenizer data
+TOKENIZER_JSON, TOKENIZER_HASH = prepare_tokenizer_data(config["tokenizer_path"])
+
+
+# Create serving app
+app = FastAPI()
+
+###################################################
+# ENDPOINTS
+###################################################
+
+# -----------------------------------------------
+# Get Full Tokenizer
+# -----------------------------------------------
+@app.get("/pokt/tokenizer")
+def get_tokenizer():
+    l.debug("returning tokenizer data")
+    return TOKENIZER_JSON
+
+
+# -----------------------------------------------
+# Get Tokenizer Hash
+# -----------------------------------------------
+@app.get("/pokt/tokenizer-hash")
+def get_tokenizer():
+    l.debug("returning tokenizer hash")
+    return TOKENIZER_HASH
