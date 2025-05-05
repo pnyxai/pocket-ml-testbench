3ffa6c908e8dc4cef23c73985660bbab984aa1c2
Ramiro R. C. <rawthil@gmail.com>
2025-05-05 17:16:11 -0300

    cleaning... (#136)

--- apps/nodejs/web/app/utilities.ts
+++
@@ -1,4 +0,0 @@
-export async function getData() {
-  // revalidate to revalidate the data every 60 secs
-  return fetch(process.env.API_ENDPOINT_URL!, {next: {revalidate: 60}}).then(res => res.json())
-}
--- apps/python/api/build.sh
+++
@@ -1,9 +0,0 @@
-DEFAULT_IMAGE_NAME="pocket_ml_api"
-
-# go to root directory
-cd ../../..
-
-# Build sidecar
-docker build . -f apps/python/api/Dockerfile --progress=plain --tag $DEFAULT_IMAGE_NAME:dev
-# Broadcast image name and tag
-echo "$DEFAULT_IMAGE_NAME"
