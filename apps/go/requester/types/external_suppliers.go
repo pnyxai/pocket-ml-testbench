d9d6289239eaa157630d8c2c9147908a6a4ac71a
Ramiro R. C. <rawthil@gmail.com>
2025-09-22 14:41:18 -0300

    gemini working (#177)

--- apps/go/requester/types/external_suppliers.go
+++ apps/go/requester/types/external_suppliers.go
@@ -10,6 +10,8 @@ type ExternalSupplierData struct {
 	ServiceTier         string            `json:"service_tier"`
 	TemperatureOverride float32           `json:"temperature_override"`
 	NoStop              bool              `json:"no_stop"`
+	NoSeed              bool              `json:"no_seed"`
+	CustomApiPath       string            `json:"custom_api_path"`
 
 	// TODO : Add support for these
 
